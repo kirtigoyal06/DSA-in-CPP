7ec420ba1ee1646024ae5dd40ca61f3755ac5df2
Kirti goyal <kirtigoyal907@gmail.com>
2026-01-12 19:45:42 +0530

    Create majority_element.cpp

---
+++ Arrays/majority_element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0,candidate=0;
+        for(int x:nums)
+        {
+            if(count==0)
+              candidate=x;
+            if(x==candidate)
+              count++;
+            else
+              count--;  
+        }
+        return candidate;
+    }
+};

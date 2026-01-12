c66b4f97c92d26321dec7a30c37e46e9f859719a
Kirti goyal <kirtigoyal907@gmail.com>
2026-01-12 19:41:57 +0530

    Create merge_sorted_array.cpp

---
+++ Arrays/merge_sorted_array.cpp
@@ -0,0 +1,18 @@
+// Problem: Sort an array using Merge Sort.
+// Approach: Divide the array into two halves, recursively sort each half,
+// and merge the sorted halves to get the final sorted array.
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0&&j>=0)
+        {
+            if(nums1[i]>nums2[j])
+               nums1[k--]=nums1[i--];
+            else
+              nums1[k--]=nums2[j--];
+        }
+        while(j>=0)
+         nums1[k--]=nums2[j--];
+    }
+};
